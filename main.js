e233dccb7acc54fdc8144aee8cef01e80f546f03
Shuting W <shuting.wu@kloudeats.com>
2023-07-31 00:57:17 -0400

    WIP: scene 1

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // main.js
 
 // Define the dimensions of your map visualization
-const width = 800;
-const height = 600;
+const width = 1920;
+const height = 1080;
 
 // Create an SVG container for the map
 const svg = d3.select("#chart")
@@ -17,33 +17,38 @@ d3.csv("./nytimes_covid-19-data_master_us-states.csv").then(function (data) {
         d.date = new Date(d.date);
         d.cases = +d.cases;
         d.deaths = +d.deaths;
-        // Add any other necessary data processing steps
+        d.fips = +d.fips;
     });
 
-    console.log(data);
+    var currentDate = new Date('2023-01-13');
+    var stateData = {};
 
     // Load the US map TopoJSON file
-    //   d3.json("https://d3js.org/us-10m.v1.json").then(function(us) {
-    //     // Create a GeoJSON feature collection from the TopoJSON
-    //     const usStates = topojson.feature(us, us.objects.states);
+    d3.json("https://d3js.org/us-10m.v1.json").then(function (us) {
+        // Create a GeoJSON feature collection from the TopoJSON
+        const usStates = topojson.feature(us, us.objects.states);
 
-    //     // Set up the color scale based on COVID-19 data (cases or deaths)
-    //     const colorScale = d3.scaleSequential()
-    //       .domain([0, d3.max(data, d => d.cases)]) // Adjust the domain as needed
-    //       .interpolator(d3.interpolateYlOrRd);
+        // Set up the color scale based on COVID-19 data (cases or deaths)
+        const colorScale = d3.scaleSequential()
+            .domain([0, d3.max(data, d => d.cases)]) // Adjust the domain as needed
+            .interpolator(d3.interpolateYlOrRd);
 
-    //     // Add the map to the SVG container
-    //     svg.selectAll("path")
-    //       .data(usStates.features)
-    //       .enter()
-    //       .append("path")
-    //       .attr("d", d3.geoPath())
-    //       .attr("fill", d => {
-    //         // Find the corresponding data for the state and use it to set the fill color
-    //         const stateData = data.find(item => item.state === d.properties.name);
-    //         return stateData ? colorScale(stateData.cases) : "gray";
-    //       });
+        // Add the map to the SVG container
+        svg.selectAll("path")
+            .data(usStates.features)
+            .enter()
+            .append("path")
+            .attr("d", d3.geoPath())
+            .attr("fill", d => {
+                // Find the corresponding data for the state and use it to set the fill color
+                stateData = data.find(item => item.fips == d.id && item.date.getTime() === currentDate.getTime());
 
-    //     // Add interactivity (e.g., tooltips) and other visual elements as needed
-    //   });
+                return stateData ? colorScale(stateData.cases) : "gray";
+            })
+            .append("title")
+            .text(stateData.state);
+
+        // Add interactivity (e.g., tooltips) and other visual elements as needed
+    });
+
 });
